58f1828fe5c5d1a1ab639846491328b4c49a82fd
Krystian Rosiński <krystian.rosinski@gmail.com>
2022-11-05 11:40:50 +0100

    Update SQLAlchemy

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requires = [
     # 'pyramid_debugtoolbar',
     'pyramid_retry',
     'pyramid_tm',
-    'SQLAlchemy==2.0.0b2',
+    'SQLAlchemy==2.0.0b3',
     'transaction',
     'zope.sqlalchemy',
     'psycopg[binary]',
