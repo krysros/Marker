0d3e73089fc20e82658cf5f73fc175c9c4bc0b0f
Krystian Rosiński <krystian.rosinski@gmail.com>
2022-12-18 14:19:31 +0100

    Match case

--- marker/dropdown.py
+++ marker/dropdown.py
@@ -17,33 +17,38 @@ class Dropdown:
 
     @property
     def title(self):
-        if self.typ == Dd.FILTER:
-            return "Filtruj"
-        elif self.typ == Dd.SORT:
-            return "Sortuj"
-        elif self.typ == Dd.ORDER:
-            return "Kolejność"
+        match self.typ:
+            case Dd.FILTER:
+                return "Filtruj"
+            case Dd.SORT:
+                return "Sortuj"
+            case Dd.ORDER:
+                return "Kolejność"
 
     @property
     def current_item(self):
-        if self.typ == Dd.FILTER:
-            return self._filter
-        elif self.typ == Dd.SORT:
-            return self._sort
-        elif self.typ == Dd.ORDER:
-            return self._order
+        match self.typ:
+            case Dd.FILTER:
+                return self._filter
+            case Dd.SORT:
+                return self._sort
+            case Dd.ORDER:
+                return self._order
 
     @property
     def icon(self):
-        if self.typ == Dd.FILTER:
-            return '<i class="bi bi-filter"></i>'
-        elif self.typ == Dd.SORT:
-            if self._order == "asc":
-                return '<i class="bi bi-sort-alpha-down"></i>'
-            elif self._order == "desc":
-                return '<i class="bi bi-sort-alpha-down-alt"></i>'
-        elif self.typ == Dd.ORDER:
-            if self._order == "asc":
-                return '<i class="bi bi-caret-up-fill"></i>'
-            elif self._order == "desc":
-                return '<i class="bi bi-caret-down-fill"></i>'
+        match self.typ:
+            case Dd.FILTER:
+                return '<i class="bi bi-filter"></i>'
+            case Dd.SORT:
+                match self._order:
+                    case "asc":
+                        return '<i class="bi bi-sort-alpha-down"></i>'
+                    case "desc":
+                        return '<i class="bi bi-sort-alpha-down-alt"></i>'
+            case Dd.ORDER:
+                match self._order:
+                    case "asc":
+                        return '<i class="bi bi-caret-up-fill"></i>'
+                    case "desc":
+                        return '<i class="bi bi-caret-down-fill"></i>'
