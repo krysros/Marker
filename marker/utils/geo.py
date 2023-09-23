17f3c3a4984c6879fd0b4a59eb81ec7a2a5dd557
Krystian Rosiński <krystian.rosinski@gmail.com>
2023-09-23 16:24:54 +0200

    Update geo.py

--- marker/utils/geo.py
+++ marker/utils/geo.py
@@ -6,7 +6,7 @@ import urllib.request
 
 def location(**kwargs):
     """
-    See: https://nominatim.org/release-docs/latest/api/Search/#parameters
+    See: https://nominatim.org/release-docs/latest/api/Search/
     Geocoding Policy: https://operations.osmfoundation.org/policies/nominatim/
     """
     if "q" in kwargs:
@@ -18,13 +18,10 @@ def location(**kwargs):
     try:
         with urllib.request.urlopen(url) as f:
             res = json.load(f)
-    except urllib.error.URLError:
-        loc = None
-    try:
-        fst = res[0]
-        lat = fst["lat"]
-        lon = fst["lon"]
-        loc = {"lat": float(lat), "lon": float(lon)}
-    except (IndexError, UnboundLocalError):
+            fst = res[0]
+            lat = fst["lat"]
+            lon = fst["lon"]
+            loc = {"lat": float(lat), "lon": float(lon)}
+    except (urllib.error.URLError, IndexError):
         loc = None
     return loc
