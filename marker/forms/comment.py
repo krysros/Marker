5d7a7d1536f177d200d38d60823a80b1360d1962
krysros <krystian.rosinski@gmail.com>
2025-07-25 08:24:55 +0200

    Preventing the addition of comments without content

--- marker/forms/comment.py
+++ marker/forms/comment.py
@@ -1,4 +1,5 @@
 from wtforms import Form, HiddenField, SelectField, TextAreaField
+from wtforms.validators import InputRequired
 
 from .filters import strip_filter
 from .select import PARENTS
@@ -6,7 +7,13 @@ from .ts import TranslationString as _
 
 
 class CommentForm(Form):
-    comment = TextAreaField(_("Comment"), filters=[strip_filter])
+    comment = TextAreaField(
+        _("Comment"),
+        validators=[
+            InputRequired(),
+        ],
+        filters=[strip_filter],
+    )
 
 
 class CommentSearchForm(Form):
