ba813ddd33e459d67cd16d4deefb27ac632cb54d
Krystian Rosiński <krystian.rosinski@gmail.com>
2026-02-14 18:49:39 +0100

    Update contact form

--- marker/forms/contact.py
+++ marker/forms/contact.py
@@ -1,19 +1,19 @@
 from wtforms import EmailField, FileField, Form, HiddenField, SelectField, StringField
 from wtforms.validators import DataRequired, InputRequired, Length
 
-from .filters import strip_filter, title
+from .filters import strip_filter
 from .select import PARENTS
 from .ts import TranslationString as _
 
 
 class ContactForm(Form):
     name = StringField(
-        _("Fullname"),
+        _("Name"),
         validators=[
             InputRequired(),
-            Length(min=5, max=100),
+            Length(max=100),
         ],
-        filters=[strip_filter, title],
+        filters=[strip_filter],
     )
     role = StringField(
         _("Role"),
@@ -39,14 +39,14 @@ class ContactForm(Form):
 
 
 class ContactSearchForm(Form):
-    name = StringField(_("Fullname"), filters=[strip_filter])
+    name = StringField(_("Name"), filters=[strip_filter])
     role = StringField(_("Role"), filters=[strip_filter])
     phone = StringField(_("Phone"), filters=[strip_filter])
     email = StringField(_("Email"), filters=[strip_filter])
 
 
 class ContactFilterForm(Form):
-    name = HiddenField(_("Fullname"), filters=[strip_filter])
+    name = HiddenField(_("Name"), filters=[strip_filter])
     role = HiddenField(_("Role"), filters=[strip_filter])
     phone = HiddenField(_("Phone"), filters=[strip_filter])
     email = HiddenField(_("Email"), filters=[strip_filter])
