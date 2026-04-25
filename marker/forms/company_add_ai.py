ebf491a77cb2df58d6f2824b4b4ad79d10097981
Krystian <krystian.rosinski@gmail.com>
2026-04-25 12:03:32 +0200

    Add strip filters to AI forms

--- marker/forms/company_add_ai.py
+++ marker/forms/company_add_ai.py
@@ -1,6 +1,7 @@
 from wtforms import Form, StringField
 from wtforms.validators import URL, InputRequired, Length
 
+from .filters import strip_filter
 from .ts import TranslationString as _
 
 
@@ -12,5 +13,6 @@ class CompanyAddAIForm(Form):
             Length(max=1000),
             URL(require_tld=True, message=_("Please enter a valid URL.")),
         ],
+        filters=[strip_filter],
         render_kw={"placeholder": "https://example.com", "autofocus": True},
     )
